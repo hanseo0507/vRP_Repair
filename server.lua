b16848c0918ccce3522a324f1ea01cbbf8975829
HanSeo0507 <khan3618@naver.com>
2020-05-30 05:08:11 +0900

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,21 @@
+--[[
+	# vRP_Repair
+	🔧 FiveM vRP Repair Script / 파이브엠 차량 수리 스크립트
+	
+	## 한국어 
+	원본 파일 : [https://github.com/Nikuez95/nk_repair](https://github.com/Nikuez95/nk_repair)
+	해당 스크립트를 사용하기위해서는 **ProgressBars** 스크립트가 필요합니다
+	[https://forum.cfx.re/t/release-progress-bars-1-0-standalone/526287](https://forum.cfx.re/t/release-progress-bars-1-0-standalone/526287)
+	제작자 : [Nikue95](https://github.com/Nikuez95)
+	컨버터 : [HanSeo](https://github.com/HanSeo0507)
+	## English
+	Original : [https://github.com/Nikuez95/nk_repair](https://github.com/Nikuez95/nk_repair)
+	**ProgressBars** script is required to use that script
+	[https://forum.cfx.re/t/release-progress-bars-1-0-standalone/526287](https://forum.cfx.re/t/release-progress-bars-1-0-standalone/526287)
+	Author : [Nikue95](https://github.com/Nikuez95)
+	Modified : [HanSeo](https://github.com/HanSeo0507)
+]]--
+
 local Proxy = module("vrp", "lib/Proxy")
 local Tunnel = module("vrp", "lib/Tunnel")
 
